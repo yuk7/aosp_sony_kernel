00d2345eb5a5b1498d5295c7177d4a574ccebc96
Haruka.K <yukx00@gmail.com>
2018-03-20 10:28:08 +0900

    Revert "input: touchscreen: cyttsp4/5: Do not probe cyttsp5 if cyttsp4 detected"
    This reverts commit 5d7f255cc01a790933189fc1c8ca35c88ac24dd0.

--- drivers/input/touchscreen/cyttsp4/cyttsp4_i2c.c
+++ drivers/input/touchscreen/cyttsp4/cyttsp4_i2c.c
@@ -51,11 +51,6 @@
 
 #define CY_I2C_DATA_SIZE  (3 * 256)
 
-#ifdef CONFIG_MACH_SONY_TULIP
-/* cyttsp detection */
-extern bool cyttsp_i2c_driver;
-#endif
-
 struct cyttsp4_i2c {
 	struct i2c_client *client;
 	u8 wr_buf[CY_I2C_DATA_SIZE];
@@ -317,11 +312,6 @@ static int cyttsp4_i2c_probe(struct i2c_client *client,
 	}
 
 	dev_info(dev, "%s: Successful probe %s\n", __func__, CYTTSP4_I2C_NAME);
-
-#ifdef CONFIG_MACH_SONY_TULIP
-	/* cyttsp detection */
-	cyttsp_i2c_driver = true;
-#endif
 
 	return 0;
 
